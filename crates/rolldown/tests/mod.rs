d820e053f5b8466d19bdf795ed78be3b279911b3
Yunfei He <i.heyunfei@gmail.com>
2022-07-15 09:03:30 +0800

    Moving fixtures folder

--- crates/rolldown/tests/mod.rs
+++ crates/rolldown/tests/mod.rs
@@ -1,2 +1,14 @@
 mod common;
-mod fixtures;
+
+use rolldown_core::log::enable_tracing_by_env;
+use testing_macros::fixture;
+
+use crate::common::test_fixture;
+use std::path::{Path, PathBuf};
+
+#[fixture("./tests/fixtures/rolldown/*")]
+fn js(path: PathBuf) {
+  enable_tracing_by_env();
+  tokio::runtime::Runtime::new().unwrap().block_on(test_fixture(&path));
+}
+
